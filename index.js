a7bf4c3b0cc969a5eb2d4f2dfd8a598d73ba3eaf
Anderson Leite <anderson.leite.bsi@gmail.com>
2022-05-04 16:54:57 -0300

    feat: add middleware cors

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const cors = require('cors');
 const { WebhookClient } = require('dialogflow-fulfillment')
 const router = require('./routes');
 const PORT =  process.env.PORT || '3000';
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json());
+
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
 
 app.use(
   '/webhook',
