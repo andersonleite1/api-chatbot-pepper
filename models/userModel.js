9bb60f4db610c434198663e9fdb743468000972d
Anderson Leite <anderson.leite.bsi@gmail.com>
2022-06-07 23:13:09 -0300

    feat: model - create user

---
+++ models/userModel.js
@@ -0,0 +1,48 @@
+const { readFile, writeFile } = require('fs/promises');
+
+const getUser = async () => {
+  try {
+    const textData = await readFile('data/user.json', 'utf-8');
+    const user = JSON.parse(textData);
+
+    return { code: 200, user };
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const create = async (username) => {
+  try {
+    const data = {
+      username: username
+    };
+
+    const stringData = JSON.stringify(data, null, 2);
+    
+    await writeFile('data/user.json', stringData);
+    return { code: 201 };
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const clear = async () => {
+  try {
+    const data = {
+      username: ""
+    };
+
+    const stringData = JSON.stringify(data, null, 2);
+  
+    await writeFile('data/user.json', stringData);
+    return { code: 200 };
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  getUser,
+  create,
+  clear,
+};
